56048b2773005dc50a276b58c24a6e4b45b007d8
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-06-14 13:52:25 -0400

    starts populating movie list item

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,39 @@
-import React, { useEffect } from 'react';
-import API from './utils/API';
+import React, { useState, useEffect } from "react";
+import API from "./utils/API";
+import MovieListItem from "./components/MovieListItem";
 
 const App = () => {
+  const [movie, setMovie] = useState({
+    title: "",
+    poster: "",
+    directors: "",
+    cast: "",
+    writers: "",
+  });
 
   useEffect(() => {
-    API.getMovies()
-    .then(res => console.log(res.data));
-  })
+    API.getMovies().then((res) => {
+      setMovie({
+        title: res.data.title,
+        poster: res.data.poster,
+        directors: res.data.directors,
+        cast: res.data.cast,
+        writers: res.data.writers,
+      });
+    });
+  }, []);
 
   return (
     <div>
-      <h1>Check the console ---></h1>
+      <MovieListItem
+        title={movie.title}
+        poster={movie.title}
+        directors={movie.directors}
+        cast={movie.cast}
+        writers={movie.writers}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- client/src/components/MovieListItem/index.js
+++ client/src/components/MovieListItem/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const MovieListItem = () => {
+const MovieListItem = (props) => {
   return (
     <div>
-      
+      <h3>{props.title}</h3>
+      <img src={props.poster} alt="" />
+      <p>Directed By: {props.directors}</p>
+      <p>Starring: {props.cast}</p>
+      <p>Written By: {props.writers}</p>
     </div>
-  )
-}
+  );
+};
 
 export default MovieListItem;
